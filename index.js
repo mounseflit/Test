39aedf2e7e06c2d283e990e4b99bf59b7824d915
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2025-05-24 19:33:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+// Load environment variables
+dotenv.config();
+
+
+
+// CORS middleware
+app.use((_req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+// Serve static files
+app.use(express.static('public'));
+
+
+app.get('/', (_req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
